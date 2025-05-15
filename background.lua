c7020879e7ee15ad07a4c780b7191eb3f8fbc6af
Jan Malte <jan@podszuweit.de>
2025-05-15 10:34:18 +0200

    rm comments

--- background.lua
+++ background.lua
@@ -1,18 +1,8 @@
 local background = {}
 
-function background.load()
-    -- local bgImage = love.graphics.newImage ("assets/galaxy.png")
-    -- love.graphics.draw (bgImage, 0,0)
-end
-
 function background.draw()
     local bgImage = love.graphics.newImage ("assets/galaxy.png")
-    local windowWidth, windowHeight = love.graphics.getDimensions()
     local imageWidth = bgImage:getWidth()
-    local imageHeight = bgImage:getHeight()
-
-    -- local x = (windowWidth - imageWidth)
-    -- local y = (windowHeight - imageHeight)
 
     love.graphics.draw(bgImage)
     love.graphics.draw(bgImage, imageWidth)
