115d7d8b7bc8e6c63c61205078cc8ccc52f461a7
Jan Malte <jan@podszuweit.de>
2025-05-15 12:14:41 +0200

    background is now animated - flying down

--- background.lua
+++ background.lua
@@ -1,15 +1,30 @@
 local background = {}
 local bgImage
-local imageWidth
+local imageHeight
+local scrollY = 0
+local scrollSpeed = 100
 
 function background.load()
-    bgImage = love.graphics.newImage ("assets/galaxy.png")
-    imageWidth = bgImage:getWidth()
+    bgImage = love.graphics.newImage ("assets/galaxy.jpg")
+    imageHeight = bgImage:getHeight()
+end
+
+function background.update(dt)
+    scrollY = scrollY + scrollSpeed * dt
+
+    -- Reset scroll to 0 when one full image has scrolled
+    if scrollY >= imageHeight then
+        scrollY = 0
+    end
 end
 
 function background.draw()
-    love.graphics.draw(bgImage)
-    love.graphics.draw(bgImage, imageWidth)
+    -- local imgWidth = bgImage:getWidth()
+    -- local imgHeight = bgImage:getHeight()
+
+    -- Draw the image twice to cover the gap
+    love.graphics.draw(bgImage, 0, scrollY)
+    love.graphics.draw(bgImage, 0, scrollY - bgImage:getHeight())
 end
 
-return background 
+return background
